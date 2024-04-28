e62854a6bada74b670b337c8a6d881aea887b397
MarcoAntonioWeb <world.solucoes.eletrica@hotmail.com>
2024-04-28 12:11:51 -0300

    Atualizando os restante do funcionamento

--- src/app/cadastro-usuarios/cadastro-usuarios.component.ts
+++ src/app/cadastro-usuarios/cadastro-usuarios.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../model/usuario.model';
 import { UsuarioService } from '../repository/usuario.service';
+import { AppState } from '../Store/app-state';
+import { Store } from '@ngrx/store';
+import * as fromUsuariosActions from '../Store/usuarios/usuarios.actions'
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-usuarios',
@@ -14,17 +18,23 @@ export class CadastroUsuariosComponent implements OnInit {
     idade: 0,
     perfil: ''
   }
-  constructor(private usuarioServico: UsuarioService) { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+  
   }
 
-  addUsuario(){
+  addUsuario(f: NgForm){
     if(this.model.id == 0) {
-      this.usuarioServico.addUsuario(this.model).subscribe()
+      console.log('Cadastra', this.model);
+      this.store.dispatch(fromUsuariosActions.CreateUsuario({payload:this.model}));
     } else {
       // Atualizar
+      this.store.dispatch(fromUsuariosActions.UpdatedUsuario({payload:this.model}));
+      console.log('Erro ao tentar cadastra', this.model);
     }
-  }
 
+    f.resetForm()
+  }
+    
 }
--- src/app/listar-usuarios/listar-usuarios.component.ts
+++ src/app/listar-usuarios/listar-usuarios.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { UsuarioService } from '../repository/usuario.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { UsuarioModel } from '../model/usuario.model';
 import { AppState } from '../Store/app-state';
 import { Store } from '@ngrx/store';
 import * as fromUsuariosAction from '../Store/usuarios/usuarios.actions';
+import * as fromUsuariosSelector from '../Store/usuarios/usuarios.reducers'
+import { Observable } from 'rxjs';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-listar-usuarios',
@@ -11,12 +13,42 @@ import * as fromUsuariosAction from '../Store/usuarios/usuarios.actions';
   styleUrls: ['./listar-usuarios.component.css']
 })
 export class ListarUsuariosComponent implements OnInit {
-  listaUsuarios: UsuarioModel[] = [];
-  constructor(private store: Store<AppState>) { }
+
+  usuario: UsuarioModel = {
+    id: 0,
+    nome: '',
+    idade: 0,
+    perfil: ''
+  }
+
+ 
+  //@Input() model: any;
+  
+  //listaUsuarios: UsuarioModel[] = [];
+  listaUsuarios$: Observable<UsuarioModel[]> = this.store.select(fromUsuariosSelector.getUsuarios);
+  usuario$: Observable<UsuarioModel | null> = this.store.select(fromUsuariosSelector.getUsuario);
+  constructor(private store: Store<AppState>,
+    
+  ) { }
 
   ngOnInit(): void {
    
-    this.store.dispatch(fromUsuariosAction.LoadUsuarios())
+    this.store.dispatch(fromUsuariosAction.LoadUsuarios());
+  }
+
+  editar(id: number) {
+      this.store.dispatch(fromUsuariosAction.LoadUsuario({payload:id}));
+      console.log('id SELECIONADO', id);
+
+  }
+
+  atualizarUsuario(f: NgForm) {
+    console.log(f.value)
+    this.store.dispatch(fromUsuariosAction.UpdatedUsuario({payload:f.value }));
+  }
+
+  excluir(id: number) {
+    this.store.dispatch(fromUsuariosAction.DeleteUsuario({payload:id}));
   }
 
 }
